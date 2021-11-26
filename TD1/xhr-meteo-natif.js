f75761f492ee455491627109c29c785966e8f592
Boulet-Clement <clement.boulet60510@gmail.com>
2021-11-26 08:40:18 +0100

    Début du td1

---
+++ TD1/xhr-meteo-natif.js
@@ -0,0 +1,14 @@
+// 
+// Affichage de la méteo avec OpenWeatherMap
+// 
+
+/**
+ * Envoie une requête XHR
+ * @param {string} UrlSend = URL ou route de l'API
+ */
+
+// Main
+
+document.addEventListener("DOMContentLoaded", function(){
+// On attend que le DOM soit chargé
+});
