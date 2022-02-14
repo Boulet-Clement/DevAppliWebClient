6147fa1079658cca9a25f7a9508f952c9b676f44
Clément Boulet <clement.boulet60510@gmail.com>
2022-02-14 22:07:34 +0100

    Create Home.vue

---
+++ music-brainz/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>  
+  <div>
+    <NavComponent/>
+    <RechercheComponent/>
+  </div>
+</template>
+
+<script>
+import NavComponent from '../components/NavComponent.vue';
+import RechercheComponent from '../components/RechercheComponent.vue';
+
+export default {
+  name: "Home",
+  components: {
+    NavComponent,
+    RechercheComponent,    
+  },
+};
+</script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
