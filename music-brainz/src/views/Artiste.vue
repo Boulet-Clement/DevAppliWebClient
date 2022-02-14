7a92c54b292f072a90746a41f4d00c4431da5197
Clément Boulet <clement.boulet60510@gmail.com>
2022-02-14 22:06:48 +0100

    Move

---
+++ music-brainz/src/views/Artiste.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <NavComponent/>
+      <ArtisteComponent  msg="Jean"/>
+  </div>
+</template>
+
+<script>
+
+import NavComponent from "../components/NavComponent.vue";
+import ArtisteComponent from "../components/ArtisteComponent.vue";
+
+
+export default {
+  name: "Artiste",
+  components: {
+    NavComponent,
+    ArtisteComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
