0857e92cb9954822a5f168c87fcb18a5089435ca
Clément Boulet <clement.boulet60510@gmail.com>
2022-02-14 22:07:18 +0100

    Create RechercheComponent.vue

---
+++ music-brainz/src/components/RechercheComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="recherche-component">
+    <input type="text" name="input_search" id="">
+    <input type="checkbox" name="Artiste" id="input_artiste">
+    <label for="input_artiste">Artiste</label>
+    <input type="checkbox" name="Titre" id="input_titre">
+    <label for="input_titre">Titre</label>
+    <button>Rechercher</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RechercheComponent',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
