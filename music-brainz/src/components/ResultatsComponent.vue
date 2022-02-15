2078221a852b58bdc36e9f7ab57a96446a0a0faa
Clément Boulet <clement.boulet60510@gmail.com>
2022-02-15 21:53:02 +0100

    Create ResultatsComponent.vue

---
+++ music-brainz/src/components/ResultatsComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="resultats-component">
+    
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ResultatsComponent',
+  data: function () {
+      return {
+      resultats : [],
+    }
+  },
+  created(){
+      this.resultats = this.$parent.$parent.$parent.results
+      console.log(this.resultats.artists[0].name)
+      if (this.resultats == []){//Si le tableau est vide : rediriger vers la page de recherche
+        this.$router.push({name:'Home'}) // Plutot redirect sur Resultats
+    }
+  }
+}
+
+</script>
