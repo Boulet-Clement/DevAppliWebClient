5e0866ca6b5af8ec96dab93ce661567771c8ab8e
Clément Boulet <clement.boulet60510@gmail.com>
2022-02-15 11:59:14 +0100

    Update RechercheComponent.vue

--- music-brainz/src/components/RechercheComponent.vue
+++ music-brainz/src/components/RechercheComponent.vue
@@ -1,34 +1,38 @@
 <template>
-  <div class="recherche-component">
-    <input type="text" name="input_search" id="">
-    <input type="checkbox" name="Artiste" id="input_artiste">
-    <label for="input_artiste">Artiste</label>
-    <input type="checkbox" name="Titre" id="input_titre">
-    <label for="input_titre">Titre</label>
-    <button>Rechercher</button>
+  <div class="container">
+    <div class="input-group mb-3">
+      <label class="input-group-text" for="inputGroupSelect01">Options</label>
+      <select class="form-select" id="inputGroupSelect01" v-on:change="onChangeType($event)">
+        <option value="1">Artiste</option>
+        <option value="2">Titre</option>
+      </select>
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Ecrire ici" v-model="search" aria-label="Ecrire ici" aria-describedby="button-addon2">
+        <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="handleSearchClick">Rechercher</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'RechercheComponent',
+  data: function () {
+    return {
+      selectedType: 1,
+      search : '',
+    }
+  },
+  methods: {
+    handleSearchClick(){
+      // On log les entrées
+      console.log(this.search)
+      console.log(this.selectedType)
+    },
+    onChangeType: function(e){
+        this.selectedType = e.target.value;
+        console.log(this.selectedType)
+    },
+  },
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
