016c8cbdf536b5197522d8f5560ae0f74b8b90e7
Clément Boulet <clement.boulet60510@gmail.com>
2022-02-20 13:57:01 +0100

    Ajout du component TitreResultat

--- music-brainz/src/views/Resultats.vue
+++ music-brainz/src/views/Resultats.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="container">
       <div class="row">
-        <ResultatsComponent v-for="artist in resultats.artists" :key="artist" :artist="artist" class="col-3 mt-3 mb-3" />
+
+        <div v-if="type == 1">
+          <h1>Artiste(s)</h1>
+          <ArtistResultatsComponent v-for="artist in resultats.artists" :key="artist" :artist="artist" class="col-12 mt-3 mb-3" />
+        </div>
+        <div v-else-if="type == 2">
+          <h1>Titre(s)</h1>
+          <TitreResultatsComponent v-for="artist in resultats.artists" :key="artist" :artist="artist" class="col-12 mt-3 mb-3" />
+        </div>
+        <div v-else>
+          Une Erreur est survenue lors du rendu des résultats
+        </div>
       </div>
   </div>
 </template>
@@ -9,25 +20,32 @@
 <script>
 
 
-import ResultatsComponent from "../components/ResultatsComponent.vue";
+import ArtistResultatsComponent from "../components/ArtistResultatsComponent.vue";
+import TitreResultatsComponent from "../components/TitreResultatsComponent.vue";
 
 export default {
   name: "Resultats",
   components: {
-    ResultatsComponent,
+    ArtistResultatsComponent,
+    TitreResultatsComponent,
   },
   data: function () {
       return {
+      type : 1, // 1 : Artiste, 2 : Titre
       resultats : [],
     }
   },
   created(){
-      this.resultats = this.$parent.$parent.results
-      //console.log(this.resultats.artists[0].name)
-      console.log(this.resultats)
-      if (this.resultats.artists == null){//Si le tableau est vide : rediriger vers la page de recherche
+      if (!this.$parent.$parent.results){//Si le tableau est vide : rediriger vers la page de recherche
         this.$router.push({name:'Home'})
-    }
+      }
+      this.type = this.$parent.$parent.type
+      this.resultats = this.$parent.$parent.results
+      if (this.resultats == null){//Si le tableau est vide : rediriger vers la page de recherche
+        this.$router.push({name:'Home'})
+      }else if (this.resultats.artists == null){//Si le tableau est vide : rediriger vers la page de recherche
+        this.$router.push({name:'Home'})
+      }
   }
 };
 </script>
