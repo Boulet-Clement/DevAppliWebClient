7ab5c3a97549c1dc4b37f0f756ab967af8b5a43e
Clément Boulet <clement.boulet60510@gmail.com>
2022-02-22 09:06:00 +0100

    Début de template de titre

--- music-brainz/src/components/TitreResultatsComponent.vue
+++ music-brainz/src/components/TitreResultatsComponent.vue
@@ -2,10 +2,10 @@
     <div id="resultats-component">
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">{{ artist.name }}</h5>
-          <h6 class="card-subtitle mb-2 text-muted" v-if="artist.area">{{ artist.area.name }}</h6>
-          <p class="card-text"> Tags :  <span v-for="tag in artist.tags" :key="tag"> <br/>- {{ tag.name }} </span></p>
-          <button class="btn btn-primary" @click="handleClickViewMore(artist)">Voir plus</button>
+          <h5 class="card-title">{{ titre.title }}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">{{ this.date }}</h6>
+          <p class="card-text"> Artistes :  <span v-for="artiste in this.artistes" :key="artiste"> {{ artiste.name }} {{ artiste.joinphrase }} </span></p>
+          <button class="btn btn-primary" @click="handleClickViewMore(titre)">Voir plus</button>
         </div>
       </div>  
     </div>
@@ -14,13 +14,24 @@
 <script>
 export default {
   name: 'ResultatsComponent',
-  props: ['artist'],
+  props: ['titre'],
   methods: {
-    handleClickViewMore(artist){
-      this.$parent.$parent.$parent.clickedArtist = artist
-      this.$router.push({name:'Artiste'})
+    handleClickViewMore(titre){
+      this.$parent.$parent.$parent.clickedtitre = titre
+      //this.$router.push({name:'Titre'})
     }
   },
+  data: function () {
+      return {
+      artistes : '',
+      date: '',
+    }
+  },
+  created(){
+      this.artistes = this.titre["artist-credit"]
+      this.date = this.titre["first-release-date"]
+      console.log(this.date)
+  }
 }
 
 </script>
