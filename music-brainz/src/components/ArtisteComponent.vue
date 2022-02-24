847ee2b7a0d7f688e79aa459f7f1e82d7cfe92b6
Clément Boulet <clement.boulet60510@gmail.com>
2022-02-24 22:33:49 +0100

    Artiste template

--- music-brainz/src/components/ArtisteComponent.vue
+++ music-brainz/src/components/ArtisteComponent.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="artiste-component">
-    <h1>{{ artist.name }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      
-    </p>
-    <button class="btn btn-primary" @click="handleClickReturnResults()">Retourner aux résultats</button>
+    <div class="container">
+      <h1> {{ artist.name }}</h1>
+      <h2 v-if="artist.area" class="ms-2 mb-2 text-muted">{{ artist.area.name }}</h2>
+      <div id="description" class="ms-3"> 
+        <span>{{ this.phraseVie }}</span>
+        <p class="card-text"> Tags :  <span v-for="tag in artist.tags" :key="tag"> <br/>- {{ tag.name }} </span></p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,5 +20,22 @@ export default {
       this.$router.push({name:'Resultats'})
     }
   },
+  data: function () {
+      return {
+      naissance : '',
+      mort: '',
+      phraseVie : '',
+      date: '',
+    }
+  },
+  created(){
+      this.naissance = this.artist["life-span"].begin
+      if (this.artist["life-span"].ended === true){
+        this.mort = this.artist["life-span"].end
+        this.phraseVie = `Naissance : ${this.naissance}, mort : ${this.mort}.`
+      }else{
+        this.phraseVie = `Naissance : ${this.naissance}.`
+      }
+  }
 }
 </script>
