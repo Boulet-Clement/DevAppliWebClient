8edd4dd6088f3fab0887a44d032fd441654ca397
Clément Boulet <clement.boulet60510@gmail.com>
2022-02-24 22:34:46 +0100

    Create Titre.vue

---
+++ music-brainz/src/views/Titre.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <TitreComponent  :titre=this.titre /> 
+  </div>
+</template>
+
+<script>
+
+import TitreComponent from "../components/TitreComponent.vue";
+
+export default {
+  name: "Titre",
+  components: {
+    TitreComponent,
+  },
+  data: function () {
+      return {
+      titre : [],
+    }
+  },
+  created(){
+      this.titre = this.$parent.$parent.clickedTitle
+      //console.log(this.resultats.artists[0].name)
+      console.log(this.titre)
+      if (this.titre == null){//Si le tableau est vide : rediriger vers la page de recherche
+        this.$router.push({name:'Home'})
+    }
+  }
+
+};
+</script>
