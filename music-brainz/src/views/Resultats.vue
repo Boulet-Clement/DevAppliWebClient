7c775f1fbb238ee50b36ba76597988a5cd302107
Clément Boulet <clement.boulet60510@gmail.com>
2022-02-24 23:00:53 +0100

    Nettoyage du code

--- music-brainz/src/views/Resultats.vue
+++ music-brainz/src/views/Resultats.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="container">
-      <div class="row">
-
-        <div v-if="type == 1">
-          <h1>Artiste(s)</h1>
-          <ArtistResultatsComponent v-for="artist in resultats.artists" :key="artist" :artist="artist" class="col-12 mt-3 mb-3" />
-        </div>
-        <div v-else-if="type == 2">
-          <h1>Titre(s)</h1>
-          <TitreResultatsComponent v-for="titre in resultats.recordings" :key="titre" :titre="titre" class="col-12 mt-3 mb-3" />
-        </div>
-        <div v-else>
-          Une Erreur est survenue lors du rendu des résultats
-        </div>
+    <div class="row">
+      <div v-if="type == 1">
+        <h1>Artiste(s)</h1>
+        <ArtistResultatsComponent
+          v-for="artist in resultats.artists"
+          :key="artist"
+          :artist="artist"
+          class="col-12 mt-3 mb-3"
+        />
       </div>
+      <div v-else-if="type == 2">
+        <h1>Titre(s)</h1>
+        <TitreResultatsComponent
+          v-for="titre in resultats.recordings"
+          :key="titre"
+          :titre="titre"
+          class="col-12 mt-3 mb-3"
+        />
+      </div>
+      <div v-else>Une Erreur est survenue lors du rendu des résultats</div>
+    </div>
   </div>
 </template>
 
 <script>
-
-
 import ArtistResultatsComponent from "../components/ArtistResultatsComponent.vue";
 import TitreResultatsComponent from "../components/TitreResultatsComponent.vue";
 
@@ -30,25 +35,25 @@ export default {
     TitreResultatsComponent,
   },
   data: function () {
-      return {
-      type : 1, // 1 : Artiste, 2 : Titre
-      resultats : [],
+    return {
+      type: 1, // 1 : Artiste, 2 : Titre
+      resultats: [],
+    };
+  },
+  created() {
+    if (!this.$parent.$parent.results) {
+      //Si le tableau n'existe pas : rediriger vers la page de recherche
+      this.$router.push({ name: "Home" });
+    }
+    this.type = this.$parent.$parent.type;
+    this.resultats = this.$parent.$parent.results;
+    if (this.resultats == null) {
+      //Si le tableau est null : rediriger vers la page de recherche
+      this.$router.push({ name: "Home" });
+    } else if (this.resultats.count == 0) {
+      //Si il n'y a pas de résultat : Envoyer sur la page sans resultat
+      this.$router.push({ name: "NoResult" });
     }
   },
-  created(){
-      if (!this.$parent.$parent.results){//Si le tableau est vide : rediriger vers la page de recherche
-        this.$router.push({name:'Home'})
-      }
-      this.type = this.$parent.$parent.type
-      this.resultats = this.$parent.$parent.results
-      console.log(this.resultats)
-      if (this.resultats == null){//Si le tableau est vide : rediriger vers la page de recherche
-      console.log("redirection ici")
-        this.$router.push({name:'Home'})
-        
-      }else if (this.resultats.artists == null && this.resultats.recordings == null){//Si le tableau est vide : rediriger vers la page de recherche
-        this.$router.push({name:'Home'})
-      }
-  }
 };
 </script>
